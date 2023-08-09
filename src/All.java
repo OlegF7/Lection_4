341e7832429c11a43864179ca262eeb08c8d2e8e
OlegF7 <novos1@mail.ru>
2023-08-09 17:17:45 +0700

    Добавляем новую практику по 4 лекции

---
+++ src/All.java
@@ -0,0 +1,51 @@
+import java.util.IllegalFormatCodePointException;
+
+class Person{
+    private String name;
+    private String middleName;
+    private String familyName;
+
+    Person(String name, String familyName, String middleName){
+        this.name = name;
+        this.middleName = middleName;
+        this.familyName = familyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public String getFamilyName() {
+        return familyName;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+@Override
+    public String toString(){
+    return name + " " + middleName + " " + familyName;
+    }
+}
+
+
+public class All {
+    public static void main(String[] args){
+        Person person = new Person("John", "Dow", "Junior");
+        System.out.println(person.toString());
+
+        person.setMiddleName("William");
+        System.out.println(person.getMiddleName());
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean repeat = true;
+        String name = "";
+        int i = 0; // переменная для отслеживания количества попыток
+        while (repeat && i < 3) {
+            System.out.print("Привет! Как тебя зовут? ");
+            String a = scan.nextLine();
+            int a1 = a.length();
+            if (a1 != 0) {
+                System.out.printf("Привет! %s", a);
+                name = a;
+                repeat = false; // Команда для выхода из цикла
+            } else {
+                System.out.println("Почему вы не ввели имя?");
+                i++; // увеличиваем счетчик попыток
+            }
+
+        }
+        System.out.println("\nПока " + name + " больше сюда не пиши.");
+        scan.close();
+    }
+}
