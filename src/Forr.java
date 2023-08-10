1640453cd6ea04172f4c26c240fae6f59b303b83
OlegF7 <novos1@mail.ru>
2023-08-10 15:14:11 +0700

    Упростили задачу с циклом for

---
+++ src/Forr.java
@@ -0,0 +1,10 @@
+public class Forr {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
