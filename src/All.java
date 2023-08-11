b1d13acca09ddda8f0d7ba4379b1eeee7cd076d2
OlegF7 <novos1@mail.ru>
2023-08-11 15:54:02 +0700

    Добавили поле age

--- src/All.java
+++ src/All.java
@@ -1,44 +1,57 @@
 import java.util.IllegalFormatCodePointException;
 
-class Person{
+class Person {
     private String name;
     private String middleName;
     private String familyName;
+    private final int age;
 
-    Person(String name, String familyName, String middleName){
+    Person(String name, String familyName, String middleName, int age) {
         this.name = name;
         this.middleName = middleName;
         this.familyName = familyName;
+        this.age = age;
     }
+
     public String getName() {
         return name;
     }
+    public int getAge() {
+        return age;
+    }
+
     public String getMiddleName() {
         return middleName;
     }
+
     public String getFamilyName() {
         return familyName;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setMiddleName(String middleName) {
         this.middleName = middleName;
     }
+
     public void setFamilyName(String familyName) {
         this.familyName = familyName;
     }
+    public void setAge() {
+    }
 
-@Override
-    public String toString(){
-    return name + " " + middleName + " " + familyName;
+    @Override
+    public String toString() {
+        return name + " " + middleName + " " + familyName + " " + age;
     }
 }
 
 
 public class All {
-    public static void main(String[] args){
-        Person person = new Person("John", "Dow", "Junior");
+    public static void main(String[] args) {
+        Person person = new Person("John", "Dow", "Junior", 35);
         System.out.println(person.toString());
 
         person.setMiddleName("William");
