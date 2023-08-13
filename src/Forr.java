1209f0cca6e904f2da9675ae5ecaeed005b22858
OlegF7 <novos1@mail.ru>
2023-08-13 10:20:01 +0700

    Вынесена отдельная функция определния крастности четверке

--- src/Forr.java
+++ src/Forr.java
@@ -1,10 +1,17 @@
 public class Forr {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 4 == 0) {
-                System.out.println(i);
-            }
+        for (int i = 100; i >= 1; i--) {
+            multiplicityСheck(i);
         }
     }
+
+    public static void multiplicityСheck(int i){
+        if (i % 4 == 0) {
+            System.out.println(i);
+        }
+    }
+
+
+
 }
